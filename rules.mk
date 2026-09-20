022fccba3c803b1260e84fed383be4f5691bafe1
JuanShotLC <38728809+JuanShotLC@users.noreply.github.com>
2026-09-20 01:21:35 -0400

    Update rules for RGBLIGHT and feature enables
    Disable RGBLIGHT and enable RGB_MATRIX, WPM, LTO, and CONSOLE features.

--- rules.mk
+++ rules.mk
@@ -1,30 +1,9 @@
-# rules.mk no esta en lenguaje C
-
-# kb: crkbd
-# km: neoncornecatdogwin
-
 OLED_ENABLE = yes
-
-OLED_DRIVER = SSD1306
-
-RGBLIGHT_ENABLE = yes
-
-TAP_DANCE_ENABLE = no
-# necesario para ocupar el doble pulso (tap dance) en el keymap
-
-EXTRAKEY_ENABLE = no
-# EXTRAKEY_ENABLE es necesario para ocupar los keycodes KC_MUTE, KC_VOLD y KC_VOLU
-
-MOUSEKEY_ENABLE = no
-
+RGBLIGHT_ENABLE = no
+RGB_MATRIX_ENABLE = yes
 WPM_ENABLE = yes
-# necesario para ocupar get_current_wpm()
-# obtener el valor de words per minute
-
 LTO_ENABLE = yes
-# Link Time Optimization reduce size al hex
-
-EXTRAFLAGS += -flto
-# esta linea reduce considerable size al hex
-
+TAP_DANCE_ENABLE = no
+EXTRAKEY_ENABLE = no
+MOUSEKEY_ENABLE = no
 CONSOLE_ENABLE = no
